d616f1faecddb5cf4a54dc1fb931e593e8e480c2
shutoskate <wizwizwayne@gmail.com>
2020-02-23 13:42:23 +0900

    index view
    [what]
    簡易的な見た目作成
    headerとmain作成

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,10 @@
 ActiveRecord::Schema.define(version: 2020_02_23_002417) do
 
   create_table "problems", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "code"
-    t.string "cause"
-    t.string "solution"
+    t.string "title"
+    t.text "code"
+    t.text "cause"
+    t.text "solution"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
